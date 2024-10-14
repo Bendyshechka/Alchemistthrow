f646a39f7324d3fe48a242984d2538b557efbe61
Bendyshechka <sus003707@gmail.com>
2024-10-14 20:34:40 +0500

    Create alchemist.lua

---
+++ alchemist.lua
@@ -0,0 +1,9 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+local Window = OrionLib:MakeWindow({Name = "Slap Battles Алхемист👽👽👽", HidePremium = false, SaveConfig = false})
+
+local Tab = Window:MakeTab({
+	Name = "Основное",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = false
+})
